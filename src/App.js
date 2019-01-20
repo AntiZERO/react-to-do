1d250cceb85dffb0244d714ac6575060bd109781
Gordon Ray <gray2884@gmail.com>
2019-01-20 15:51:17 -0600

    Checkpoint 2752 Completed

--- src/App.js
+++ src/App.js
@@ -10,18 +10,41 @@ class App extends Component {
           { description: 'Walk the cat', isCompleted: true },
           { description: 'Throw the dishes away', isCompleted: false },
           { description: 'Buy new dishes', isCompleted: false }
-      ]
+      ],
+      newTodoDescription: ''
     };
   }
+
+  handleChange(e){
+    this.setState({ newTodoDescription: e.target.value })
+  }
+
+handleSubmit(e) {
+  e.preventDefault();
+  if (!this.state.newTodoDescription) { return }
+  const newTodo = { description: this.state.newTodoDescription, isCompleted: false };
+  this.setState({ todos: [...this.state.todos, newTodo], newTodoDescription: '' });
+}
+
+toggleComplete(index) {
+  const todos=this.state.todos.slice();
+  const todo = todos[index];
+  todo.isCompleted = todo.isCompleted ? false : true;
+  this.setState({ todos: todos });
+}
 
   render() {
     return (
       <div className="App">
         <ul>
           { this.state.todos.map( (todo, index) =>
-            <ToDo key={ index } description={ todo.description } isCompleted={ todo.isCompleted } />
+            <ToDo key={ index } description={ todo.description } isComplete={ todo.isCompleted } toggleComplete={ () => this.toggleComplete(index) } />
             )}
         </ul>
+        <form onSubmit={ (e) => this.handleSubmit(e) }>
+          <input type="text" value={ this.state.newTodoDescription } onChange={ (e) => this.handleChange(e) } />
+          <input type="submit" />
+        </form>
       </div>
     );
   }
--- src/components/ToDo.js
+++ src/components/ToDo.js
@@ -4,7 +4,7 @@ class ToDo extends Component {
 	render() {
 		return (
 			<li>
-				<input type="checkbox" checked={ this.props.isCompleted } />
+				<input type="checkbox" checked={ this.props.isCompleted } onChange={ this.props.toggleComplete } />
 				<span>{ this.props.description }</span>
 			</li>
 			);
